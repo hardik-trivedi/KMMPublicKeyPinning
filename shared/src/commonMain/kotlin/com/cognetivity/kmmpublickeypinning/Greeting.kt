f14e4ff95bf7dc608836f0ba2b012696e4e60705
hardik-trivedi <hardik.trivedi_11@yahoo.com>
2022-08-10 10:34:39 +0100

    first commit

---
+++ shared/src/commonMain/kotlin/com/cognetivity/kmmpublickeypinning/Greeting.kt
@@ -0,0 +1,7 @@
+package com.cognetivity.kmmpublickeypinning
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/cognetivity/kmmpublickeypinning/Platform.kt
@@ -0,0 +1,5 @@
+package com.cognetivity.kmmpublickeypinning
+
+expect class Platform() {
+    val platform: String
+}
